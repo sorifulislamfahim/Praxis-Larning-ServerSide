9aa518c6adec25ea3b4b8b407c3dc3f04d07992c
Soriful Islam Fahim <islamsoriful248@gmail.com>
2022-10-26 22:51:10 +0600

    update courses data

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require('express')
-const app = express()
+const app = express();
+const cors = require('cors');
 const port = process.env.PORT || 5000;
 
 const courses = require('./data/courses.json');
+const courseNews = require('./data/course-news.json')
+
+app.use(cors())
 
 app.get('/', (req, res) => {
     res.send('This Server Is Running')
@@ -10,6 +14,18 @@ app.get('/', (req, res) => {
 
   app.get('/all-courses', (req, res) => {
     res.send(courses)
+  })
+
+  app.get('/courses/:id', (req, res) => {
+    const id = req.params.id;
+    const coursesNews = courseNews.filter( courses => courses.category_id = id)
+    res.send(courseNews);
+  })
+
+  app.get('/course/:id', (req, res) => {
+    const id = req.params.id;
+    const selectedCourse = courseNews.find( course => course._id = id)
+    res.send(selectedCourse);
   })
 
 
